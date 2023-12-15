7d642f50e9194671671ba55435eab027b32e0b95
prajwal-peepultree <prajwal@peepultree.world>
2023-12-15 17:30:32 +0530

    adding vars

--- app/components/Organisms/HomeComponents/NavBar/Nav.jsx
+++ app/components/Organisms/HomeComponents/NavBar/Nav.jsx
@@ -6,7 +6,7 @@ import Hamburger_Icon from "../../../../assets/Hamburger_Icon.svg";
 import Close_Icon_SC from "../../../../assets/Close_Icon_SC.svg";
 import Image from 'next/image';
 
-export default function Nav() {
+export default function Nav({searchActive,setSearchActive}) {
   return (
     <div className='flex w-full justify-between align-middle items-center py-5'>
         <span className='font-bold text-xl'>Hello, Ram Krishan</span>
@@ -14,8 +14,8 @@ export default function Nav() {
           <button>
             <Image src={Phone_Icon} alt="Phone"/>
           </button>
-          <button>
-            <Image src={Search_Icon} alt="Search"/>
+          <button onClick={()=>{setSearchActive(!searchActive)}}>
+            {searchActive ? <Image src={Close_Icon_SC} alt="Close"/> : <Image src={Search_Icon} alt="Search"/>}
           </button>
           <button>
             <Image src={Hamburger_Icon} alt="Hamburger"/>
--- app/home/home.jsx
+++ app/home/home.jsx
@@ -1,13 +1,19 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../components/Organisms/HomeComponents/NavBar/Nav'
 import Search from '../components/Organisms/HomeComponents/SearchBox/Search';
 
 export default function home() {
+    const [searchActive, setSearchActive] = useState(false);
+    const [searchText, setSearchText] = useState();
+
+    function getSearchBox() {
+        return(searchActive && <Search searchText={searchText} setSearchText={setSearchText}/>)
+    }
   return (
     <div className="min-h-screen h-full max-w-md px-[22px] lg:mx-auto justify-start align-middle items-center bg-Background-primary text-center flex flex-col">
-        <Nav/>
-        <Search/>
+        <Nav searchActive={searchActive} setSearchActive={setSearchActive}/>
+        {getSearchBox()} 
     </div>
   )
 }
